d3b9c2d88109e32cf8d313b21583ba6cf8973e86
shagaba <shai.gabai@shagaba.com>
2016-09-05 22:19:31 +0300

    'REMOVE' & 'REPLACE' test implementation

---
+++ src/test/java/com/shagaba/jacksonpatch/ReplaceOperationTest.java
@@ -0,0 +1,160 @@
+package com.shagaba.jacksonpatch;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.shagaba.jacksonpatch.ReplaceOperation;
+import com.shagaba.jacksonpatch.PatchOperation;
+import com.shagaba.jacksonpatch.post.dto.Author;
+import com.shagaba.jacksonpatch.post.dto.Post;
+import com.shagaba.jacksonpatch.post.dto.Section;
+
+public class ReplaceOperationTest {
+	
+	private ObjectMapper mapper = null;
+
+    public ObjectMapper newObjectMapper() {
+        ObjectMapper jacksonObjectMapper = new ObjectMapper();
+
+        // - SerializationFeature for changing how JSON is written
+
+        // to allow serialization of "empty" POJOs (no properties to serialize)
+        // (without this setting, an exception is thrown in those cases)
+        jacksonObjectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+        // Feature that determines whether Map entries with null values are to be serialized (true) or not (false)
+        jacksonObjectMapper.disable(SerializationFeature.WRITE_NULL_MAP_VALUES);
+        // only properties with non-null
+        jacksonObjectMapper.setSerializationInclusion(Include.NON_NULL);
+
+        // - DeserializationFeature for changing how JSON is read as POJOs:
+
+        // to prevent exception when encountering unknown property:
+        jacksonObjectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+
+        // default: java.util.Date serialized as textual (ISO-8601) values
+        return jacksonObjectMapper;
+    }
+    
+    @Before
+    public void beforeEach() {
+    	mapper = newObjectMapper();
+    	
+    }
+
+    @Test
+    public void replaceTitle() throws Exception {
+    	Post postV1 = new Post();
+    	postV1.setTitle("1stTitle");
+        JsonNode postV1Node = mapper.valueToTree(postV1);
+
+    	String title = "my test title";
+        ReplaceOperation replaceOperation = new ReplaceOperation("/title", mapper.valueToTree(title));
+        String replaceValueJson = mapper.writeValueAsString(replaceOperation);
+
+        // read action
+        PatchOperation action = mapper.readValue(replaceValueJson, PatchOperation.class);
+        JsonNode postV2Node = action.apply(postV1Node);
+        Post postV2 = mapper.treeToValue(postV2Node, Post.class);
+        
+        Assert.assertThat(postV2.getTitle(), equalTo(title));
+    }
+
+    @Test
+    public void replaceAuthor() throws Exception {
+    	Author authorV1 = new Author("harry", "potter", "harry.potter@wizard.com");
+    	Post postV1 = new Post();
+    	postV1.setAuthor(authorV1);
+        JsonNode postV1Node = mapper.valueToTree(postV1);
+
+    	Author author = new Author("james", "bond", "james.bond@007.com");
+        ReplaceOperation replaceOperation = new ReplaceOperation("/author", mapper.valueToTree(author));
+        String replaceValueJson = mapper.writeValueAsString(replaceOperation);
+
+        // read action
+        PatchOperation action = mapper.readValue(replaceValueJson, PatchOperation.class);
+        JsonNode postV2Node = action.apply(postV1Node);
+        Post postV2 = mapper.treeToValue(postV2Node, Post.class);
+        
+        Assert.assertThat(postV2.getAuthor(), equalTo(author));
+    }
+
+    @Test
+    public void replaceAuthorFirstName() throws Exception {
+    	Author authorV1 = new Author("harry", "potter", "harry.potter@wizard.com");
+    	Post postV1 = new Post();
+       	postV1.setAuthor(authorV1);
+        JsonNode postV1Node = mapper.valueToTree(postV1);
+
+        String firstName = "james";
+        ReplaceOperation replaceOperation = new ReplaceOperation("/author/firstName", mapper.valueToTree(firstName));
+        String replaceValueJson = mapper.writeValueAsString(replaceOperation);
+
+        // read action
+        PatchOperation action = mapper.readValue(replaceValueJson, PatchOperation.class);
+        JsonNode postV2Node = action.apply(postV1Node);
+        Post postV2 = mapper.treeToValue(postV2Node, Post.class);
+
+        Assert.assertThat(postV2.getAuthor().getFirstName(), equalTo(firstName));
+        Assert.assertThat(postV2.getAuthor().getLastName(), equalTo(authorV1.getLastName()));
+        Assert.assertThat(postV2.getAuthor().getEmail(), equalTo(authorV1.getEmail()));
+    }
+
+    @Test
+    public void replaceMiddleSection() throws Exception {
+    	Post postV1 = new Post();
+    	postV1.setSections(new ArrayList<Section>());
+    	postV1.getSections().add(new Section("section-1", null));
+    	postV1.getSections().add(new Section("section-2", null));
+    	postV1.getSections().add(new Section("section-3333", null));
+        postV1.getSections().add(new Section("section-4", null));
+        postV1.getSections().add(new Section("section-5", null));
+        JsonNode postV1Node = mapper.valueToTree(postV1);
+
+        Section section3 = new Section("section-3", null);
+        ReplaceOperation replaceOperation = new ReplaceOperation("/sections/2", mapper.valueToTree(section3));
+        String replaceValueJson = mapper.writeValueAsString(replaceOperation);
+
+        // read action
+        PatchOperation action = mapper.readValue(replaceValueJson, PatchOperation.class);
+        JsonNode postV2Node = action.apply(postV1Node);
+        Post postV2 = mapper.treeToValue(postV2Node, Post.class);
+
+        Assert.assertThat(postV2.getSections().size(), equalTo(5));
+        Assert.assertThat(postV2.getSections().get(2), equalTo(section3));
+    }
+
+    @Test
+    public void replaceLastSection() throws Exception {
+    	Post postV1 = new Post();
+    	postV1.setSections(new ArrayList<Section>());
+    	postV1.getSections().add(new Section("section-1", null));
+    	postV1.getSections().add(new Section("section-2", null));
+    	postV1.getSections().add(new Section("section-3", null));
+        postV1.getSections().add(new Section("section-4", null));
+        postV1.getSections().add(new Section("section-5555", null));
+        JsonNode postV1Node = mapper.valueToTree(postV1);
+
+        Section section5 = new Section("section-5", null);
+        ReplaceOperation replaceOperation = new ReplaceOperation("/sections/4", mapper.valueToTree(section5));
+        String replaceValueJson = mapper.writeValueAsString(replaceOperation);
+
+        // read action
+        PatchOperation action = mapper.readValue(replaceValueJson, PatchOperation.class);
+        JsonNode postV2Node = action.apply(postV1Node);
+        Post postV2 = mapper.treeToValue(postV2Node, Post.class);
+
+        Assert.assertThat(postV2.getSections().size(), equalTo(5));
+        Assert.assertThat(postV2.getSections().get(4), equalTo(section5));
+    }
+
+}
