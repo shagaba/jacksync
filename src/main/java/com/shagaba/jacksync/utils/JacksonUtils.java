dcb126a88389e518967288a43d5f83b04de74aa7
shagaba <shai.gabai@shagaba.com>
2016-09-23 13:52:17 +0300

    add merge between two JsonNodes

--- src/main/java/com/shagaba/jacksync/utils/JacksonUtils.java
+++ src/main/java/com/shagaba/jacksync/utils/JacksonUtils.java
@@ -1,8 +1,12 @@
 package com.shagaba.jacksync.utils;
 
+import java.util.Iterator;
+
 import com.fasterxml.jackson.core.JsonPointer;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.base.Strings;
 import com.shagaba.jacksync.exception.IllegalContainerException;
 import com.shagaba.jacksync.exception.NoSuchPathException;
@@ -156,4 +160,44 @@ public class JacksonUtils {
 		return path.append(JsonPointer.compile(SEPARATOR.concat(fieldName)));
 	}
 
+	/**
+	 * 
+	 * @param mainNode
+	 * @param updateNode
+	 * @return
+	 */
+	public static JsonNode merge(JsonNode mainNode, JsonNode updateNode) {
+
+	    Iterator<String> fieldNames = updateNode.fieldNames();
+
+	    while (fieldNames.hasNext()) {
+	        String updatedFieldName = fieldNames.next();
+	        JsonNode valueToBeUpdated = mainNode.get(updatedFieldName);
+	        JsonNode updatedValue = updateNode.get(updatedFieldName);
+
+	        // ArrayNode
+	        if (valueToBeUpdated != null && updatedValue.isArray()) {
+	            // running a loop for all elements of the updated ArrayNode
+	            for (int i = 0; i < updatedValue.size(); i++) {
+	                JsonNode updatedChildNode = updatedValue.get(i);
+	                // Create a new Node in the node that should be updated, if there was no corresponding node in it
+	                // Use-case - where the updateNode will have a new element in its Array
+	                if (valueToBeUpdated.size() <= i) {
+	                    ((ArrayNode) valueToBeUpdated).add(updatedChildNode);
+	                }
+	                // getting reference for the node to be updated
+	                JsonNode childNodeToBeUpdated = valueToBeUpdated.get(i);
+	                merge(childNodeToBeUpdated, updatedChildNode);
+	            }
+	        // ObjectNode
+	        } else if (valueToBeUpdated != null && valueToBeUpdated.isObject()) {
+	            merge(valueToBeUpdated, updatedValue);
+	        } else {
+	            if (mainNode instanceof ObjectNode) {
+	                ((ObjectNode) mainNode).replace(updatedFieldName, updatedValue);
+	            }
+	        }
+	    }
+	    return mainNode;
+	}
 }
